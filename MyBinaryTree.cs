a6e88b1b95051964c312da46b765a85263f50522
tejaswini kakade <92703176+tejaswinikakade@users.noreply.github.com>
2021-11-09 01:32:46 +0530

    Add files via upload

---
+++ MyBinaryTree.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Serch_Tree_Day15
+{
+    class MyBinaryTree<T> where T : IComparable<T>
+    {
+        public T Node { get; set; }
+        public MyBinaryTree<T> leftNode { get; set; }
+        public MyBinaryTree<T> rightNode { get; set; }
+
+        public MyBinaryTree(T nodedata)
+        {
+            this.Node = nodedata;
+            this.rightNode = null;
+            this.leftNode = null;
+        }
+
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        public void Add(T item)
+        {
+            T currentNodeValue = this.Node;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.leftNode == null)
+                    this.leftNode = new MyBinaryTree<T>(item);
+                else
+                    this.leftNode.Add(item);
+                Console.WriteLine(item + "Added to left");
+
+            }
+
+            else
+            {
+                if (this.rightNode == null)
+                    this.rightNode = new MyBinaryTree<T>(item);
+                else
+                    this.rightNode.Add(item);
+                Console.WriteLine(item + "Added to right");
+            }
+        }
+
+        public bool Search(T element, MyBinaryTree<T> node)
+        {
+            if (node == null)
+                return false;
+
+            if (node.Node.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.Node);
+                result = true;
+            }
+            else
+                Console.WriteLine("Did not found the element in BST" + " " + node.Node);
+
+            if (element.CompareTo(node.Node) < 0)
+                Search(element, node.leftNode);
+
+            if (element.CompareTo(node.Node) < 0)
+                Search(element, node.rightNode);
+
+            return result;
+
+        }
+
+        public void Print()
+        {
+            if (this.leftNode != null)
+            {
+                this.leftCount++;
+                this.leftNode.Print();
+
+            }
+
+            Console.WriteLine(this.Node.ToString());
+
+            if (this.rightNode != null)
+            {
+                this.rightCount++;
+                this.rightNode.Print();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Binary_Serch_Tree_Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBinaryTree<int> binaryTree = new MyBinaryTree<int>(56);
+
+            //UC1
+            binaryTree.Add(30);
+            binaryTree.Add(70);
+            binaryTree.Print();
+
+
+
+
+            //UC2
+            binaryTree.Add(30);
+            binaryTree.Add(70);
+            binaryTree.Add(22);
+            binaryTree.Add(40);
+            binaryTree.Add(60);
+            binaryTree.Add(95);
+            binaryTree.Add(11);
+            binaryTree.Add(65);
+            binaryTree.Add(3);
+            binaryTree.Add(16);
+            binaryTree.Add(63);
+            binaryTree.Add(67);
+            binaryTree.Print();
+
+
+            //UC3
+            binaryTree.Add(30);
+            binaryTree.Add(70);
+            binaryTree.Add(22);
+            binaryTree.Add(40);
+            binaryTree.Add(60);
+            binaryTree.Add(95);
+            binaryTree.Add(11);
+            binaryTree.Add(65);
+            binaryTree.Add(3);
+            binaryTree.Add(16);
+            binaryTree.Add(63);
+            binaryTree.Add(56);
+            binaryTree.Print();
+
+            bool result = binaryTree.Search(67, binaryTree);
+            Console.WriteLine(result);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
